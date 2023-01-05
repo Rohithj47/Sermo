650fdc180826c59a96406e36d7126220ecba1a69
Rahul Suresh <rahs98@gmail.com>
2023-01-04 23:52:30 -0500

    File upload to server

--- mobile-client/app/src/main/java/com/example/sermo/GenerateReportActivity.java
+++ mobile-client/app/src/main/java/com/example/sermo/GenerateReportActivity.java
@@ -9,11 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
@@ -22,11 +24,36 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GenerateReportActivity extends AppCompatActivity {
 
     ActivityResultLauncher<Intent> resultLauncher;
     ImageButton btSelect;
+
+    private static byte[] readUri(Context context, Uri uri) throws IOException {
+        ParcelFileDescriptor pdf = context.getContentResolver().openFileDescriptor(uri, "r");
+
+        assert pdf != null;
+        assert pdf.getStatSize() <= Integer.MAX_VALUE;
+        byte[] data = new byte[(int) pdf.getStatSize()];
+
+        FileDescriptor fd = pdf.getFileDescriptor();
+        FileInputStream fileStream = new FileInputStream(fd);
+        fileStream.read(data);
+
+        return data;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +78,7 @@ public class GenerateReportActivity extends AppCompatActivity {
                         File file = new File(sUri.getPath());
                         final String[] split = file.getPath().split(":");
                         String filePath = split[1];
-                        Log.d("SELECTED FILE", sPath + ", " + sPath + ", " + filePath);
+                        Log.d("SELECTED FILE", sPath + ", " + sPath + ", " + filePath + ", " + sUri);
                         Cursor returnCursor =
                                 getContentResolver().query(sUri, null, null, null, null);
                         /*
@@ -65,6 +92,33 @@ public class GenerateReportActivity extends AppCompatActivity {
                         Log.d("SELECTED FILE INFO", returnCursor.getString(nameIndex) + ", " + Long.toString(returnCursor.getLong(sizeIndex)));
                         TextView tv = findViewById(R.id.PatientInformationFilePath);
                         tv.setText(returnCursor.getString(nameIndex));
+                        File f = new File(returnCursor.getString(nameIndex));
+                        Log.d("SELECTED FILE PATH", f.getAbsolutePath());
+                        RequestBody reqBody = null;
+                        try {
+                            reqBody = RequestBody.create(MediaType.parse("multipart/form-file"), readUri(GenerateReportActivity.this, sUri));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        MultipartBody.Part partImage = MultipartBody.Part.createFormData("file", file.getName(), reqBody);
+                        API api = RetrofitClient.getInstance().getAPI();
+                        Call<ResponseBody> upload = api.uploadImage(partImage);
+                        upload.enqueue(new Callback<ResponseBody>() {
+                            @Override
+                            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                                if(response.isSuccessful()) {
+                                    Toast.makeText(GenerateReportActivity.this, "Image Uploaded", Toast.LENGTH_SHORT).show();
+                                    Intent main = new Intent(GenerateReportActivity.this, GenerateReportActivity.class);
+                                    main.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(main);
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                                Toast.makeText(GenerateReportActivity.this, "Request failed" + t.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
                 }
             });
