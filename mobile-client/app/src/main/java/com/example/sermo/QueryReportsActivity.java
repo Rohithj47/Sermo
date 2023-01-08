ea4bcdae78f2d1ea5a3690a87028e310087d5d86
Rahul Suresh <rahs98@gmail.com>
2023-01-08 00:19:47 -0500

    Search plus tags

--- mobile-client/app/src/main/java/com/example/sermo/QueryReportsActivity.java
+++ mobile-client/app/src/main/java/com/example/sermo/QueryReportsActivity.java
@@ -3,6 +3,13 @@ package com.example.sermo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.TextView;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class QueryReportsActivity extends AppCompatActivity {
 
@@ -10,5 +17,22 @@ public class QueryReportsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_reports);
+    }
+
+    public void addChipToGroup(View view) {
+        TextView textView = findViewById(R.id.searchTextView);
+        Chip chip = new Chip(QueryReportsActivity.this);
+        chip.setText(textView.getText());
+        chip.setCloseIconVisible(true);
+        chip.setOnClickListener(v -> {
+            ViewParent parent = v.getParent();
+            if (parent instanceof ViewGroup) {
+                ViewGroup group = (ViewGroup) parent;
+                group.removeView(v);
+            }
+        });
+        ChipGroup chipGroup = findViewById(R.id.searchChipGroup);
+        chipGroup.addView(chip);
+        textView.setText("");
     }
 }
